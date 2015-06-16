fdd5a653df43602697a521ad924b7a075e9f9d9a
kazet15 <kazet15@gmail.com>
2015-06-16 22:22:19 +0200

    modify camera image handling

--- server.js
+++ server.js
@@ -12,14 +12,19 @@ var port = config.get('server.port');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.writeHead(200, {
-        'content-type': 'image/jpeg',
-        'cache-control': 'private, no-cache, no-store, must-revalidate',
-        'expires': '0',
-        'pragma': 'no-cache'
-    });
+    camera.getImage(function (err, data) {
+        if (!err) {
+            res.writeHead(200, {
+                'content-type': 'image/jpeg',
+                'content-length': data.length,
+                'cache-control': 'private, no-cache, no-store, must-revalidate',
+                'expires': '0',
+                'pragma': 'no-cache'
+            });
 
-    camera.getImage().pipe(res);
+            res.end(data);
+        }
+    });
 });
 
 app.listen(port);
